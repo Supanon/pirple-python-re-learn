64ac3c8a910bf4c16b9a098271aaf3bd5a9ec15f
Supanon <45396925+Supanon@users.noreply.github.com>
2021-02-10 13:59:43 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# FAVORITE SONG
+
+Track = "Flowers in the sun" 
+Artist = "Waterfront dining"
+Genre = "Vaporwave"
+DurationInMinute = 5.17 
+OriginalSample = "Scott White - Never Ends"
+
+print(Track)
+print(Artist)
+print(Genre)
+print(DurationInMinute)
+print(OriginalSample)
